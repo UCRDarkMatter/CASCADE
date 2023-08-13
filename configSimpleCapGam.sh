10c5d750c23a6b26cd34f435e6bcb088515a38ef
Leo Weimer <111520021+leo3po2@users.noreply.github.com>
2023-08-13 16:08:32 -0700

    Update configSimpleCapGam.sh

--- configSimpleCapGam.sh
+++ configSimpleCapGam.sh
@@ -1,7 +1,7 @@
 
 #configuration file for G4SimpleCapGam, sets to optimal settings for relative intensity and correlation agreement
 
-export G4NEUTRONHP_USE_SIMPLE_CAPGAM=1
+export G4NEUTRONHP_USE_SIMPLE_CAPGAM=0
 export G4NEUTRONHP_USE_RAW_EXCITATION=0
 export G4NEUTRONHP_ALWAYS_EMIT_GAMMA=0
 
