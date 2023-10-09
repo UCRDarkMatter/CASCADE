3093a2e127393c6dea56a29603b24e6460298b85
Leo Weimer <111520021+leo3po2@users.noreply.github.com>
2023-10-09 14:25:22 -0400

    Update and rename configSimpleCapGam.sh to configCASCADE.sh
    Changed "SimpleCapGam" to "CASCADE"

---
+++ configCASCADE.sh
@@ -0,0 +1,9 @@
+
+#configuration file for G4CASCADE, sets to optimal settings for relative intensity and correlation agreement
+
+export G4NEUTRONHP_USE_CASCADE=0
+export G4NEUTRONHP_USE_RAW_EXCITATION=0
+export G4NEUTRONHP_ALWAYS_EMIT_GAMMA=0
+
+#Change this to your data directory:
+export CAPGAM_DATA_DIR=~/CASCADE/CapGamData
--- configSimpleCapGam.sh
+++
@@ -1,9 +0,0 @@
-
-#configuration file for G4SimpleCapGam, sets to optimal settings for relative intensity and correlation agreement
-
-export G4NEUTRONHP_USE_SIMPLE_CAPGAM=0
-export G4NEUTRONHP_USE_RAW_EXCITATION=0
-export G4NEUTRONHP_ALWAYS_EMIT_GAMMA=0
-
-#Change this to your data directory:
-export SIMPLE_CAPGAM_DATA_DIR=~/SimpleCapGam/CapGamData
