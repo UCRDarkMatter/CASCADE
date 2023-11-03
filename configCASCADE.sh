e74badf028f286cf2ba58d8739e3f7a07a99fd83
Leo Weimer <111520021+leo3po2@users.noreply.github.com>
2023-11-03 09:01:21 -0400

    Update configCASCADE.sh

--- configCASCADE.sh
+++ configCASCADE.sh
@@ -1,5 +1,5 @@
 
-#configuration file for G4CASCADE, sets to optimal settings for relative intensity and correlation agreement
+#configuration file for G4CASCADE
 
 export G4NEUTRONHP_USE_CASCADE=0
 export G4NEUTRONHP_USE_RAW_EXCITATION=0
