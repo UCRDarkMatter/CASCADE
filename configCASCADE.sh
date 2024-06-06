2dd0b177fa6411c5c6069d045c03b31932117ca3
Leo Weimer <111520021+leo3po2@users.noreply.github.com>
2024-06-06 11:02:35 -0400

    Delete configCASCADE.sh

--- configCASCADE.sh
+++
@@ -1,9 +0,0 @@
-
-#configuration file for G4CASCADE
-
-export G4NEUTRONHP_USE_CASCADE=0
-export G4NEUTRONHP_USE_RAW_EXCITATION=0
-export G4NEUTRONHP_ALWAYS_EMIT_GAMMA=0
-
-#Change this to your data directory:
-export CASCADE_DATA_DIR=~/CASCADE/CapGamData
